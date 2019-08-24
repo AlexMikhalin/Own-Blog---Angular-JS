5e55decc673ce1fa27c4fc86adde681fba83f0cb
unknown <sashamikhalin2017@yandex.ru>
2019-08-24 16:33:35 +0300

    Add Angilar JS files

---
+++ src/js/app/core/post/post.service.js
@@ -0,0 +1,21 @@
+'use strict';
+
+angular.
+    module('post').
+        factory('Post', function($resource){
+            var url = '/json/posts.json'
+            return $resource(url, {}, {
+                query: {
+                    method: "GET",
+                    params: {},
+                    isArray: true,
+                    cache: false,
+                },
+                get: {
+                    method: "GET",
+                    isArray: true,
+                    cache: false,
+                }
+            })
+
+        });
